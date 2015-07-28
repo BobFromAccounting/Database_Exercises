e2e3eb5d4ddc2b974d66729dbce291b4ebc7407d
Tarek S Hafez <thafez@ymail.com>
2015-07-28 15:27:41 -0500

    Modified albums_migration.sql, albums_seeder.sql, users_migration.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
- USE codeup_test_db;
+USE codeup_test_db;
 
- DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
- CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(100) NOT NULL,
-    record_name  VARCHAR(100) NOT NULL,
-    release_date YEAR(4) NOT NULL,
-    sales_in_millions DECIMAL(15, 2) NOT NULL,
-    genre CHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE albums (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   artist_name VARCHAR(100) NOT NULL,
+   record_name  VARCHAR(100) NOT NULL,
+   release_date YEAR(4) NOT NULL,
+   sales_in_millions DECIMAL(15, 2) NOT NULL,
+   genre CHAR(100) NOT NULL,
+   PRIMARY KEY (id)
 );
