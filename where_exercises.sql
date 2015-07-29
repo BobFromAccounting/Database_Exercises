5a39671f1ce3a98838ff8087bb551b9877f8822a
Tarek S Hafez <thafez@ymail.com>
2015-07-29 14:00:26 -0500

    Adding where_exercises.sql

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,26 @@
-SELECT first_name
+SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
-WHERE first_name in ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'm'
+	AND (
+	first_name = ('Irena')
+	OR first_name = ('Vidya')
+	OR first_name = ('Maya')
+	);
+
+
+SELECT concat(last_name, ' ', first_name) AS last_names_first
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT concat(first_name, ' ', last_name) AS 1990s_hires
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT concat(first_name, ' ', last_name) AS Holiday_Born
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT concat(first_name, ' ', last_name) AS Employee_Q
+FROM employees
+WHERE last_name LIKE '%q%'; 
+
