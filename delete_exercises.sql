39b590c2cdbd7073c14f3164c658a4645cc9d14b
Tarek S Hafez <thafez@ymail.com>
2015-07-29 10:50:19 -0500

    delete_exercises.sql

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,10 @@
 USE codeup_test_db;
 
 SELECT record_name, release_date FROM albums WHERE release_date > 1991;
+DELETE albums WHERE release_date > 1991;
 
-SELECT record_name FROM albums WHERE genre = 'DISCO';
+SELECT record_name FROM albums WHERE genre = 'Disco';
+DELETE albums WHERE genre = 'Disco';
 
 SELECT artist_name, record_name FROM albums WHERE artist_name = 'Whitney Houston' || artist_name = 'Bruce Springsteen';
+DELETE albums WHERE artist_name = 'Whitney Houston' || artist_name 'Bruce Springsteen';
