fcbb0459c6e2ddb23326dec31a88829f76e02beb
Tarek S Hafez <thafez@ymail.com>
2015-07-30 09:35:31 -0500

    Modified group_by_exercise.sql

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,9 +4,18 @@ SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC;
 
-SELECT concat(last_name, ' ', first_name) AS last_names_first
+SELECT last_name
 FROM employees
-WHERE last_name LIKE '%e'
-    AND last_name LIKE 'e%'
+WHERE last_name LIKE 'e%e'
 GROUP BY last_name;
 
+SELECT concat(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY full_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
